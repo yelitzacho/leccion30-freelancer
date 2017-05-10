3ceb8cd5b4db56a10bc5e78e031b1e02026fb220
Yelitza Cho <yelitza.choque@gmail.com>
2017-05-10 13:14:39 -0500

    Modal

--- js/app.js
+++ js/app.js
@@ -15,3 +15,31 @@ window.onscroll = function(){
 	}
 }
 
+window.addEventListener('load', function (evento){
+	var galeryContainer = document.getElementById('img-content');
+
+	for (var i = 1; i <= 6; i++) {
+		/*creando divs*/
+		var urlImg = 'assets/images/portfolio/'+ i +'.PNG';
+		var div = document.createElement('div');
+		var img = document.createElement('img');
+		img.setAttribute('src', urlImg);
+		img.setAttribute('class', '');
+		div.setAttribute('class','img-portfolio');
+		/*añadiendo a html*/
+		div.appendChild(img);
+		galeryContainer.appendChild(div);
+		var galeria = document.getElementById("img-content");
+		};
+		var modal = document.getElementById("modal");
+		var imgModal = document.getElementById("myImg");
+		galeria.addEventListener('click', function(e){
+			if (e.target.tagName == "IMG") {
+	        modal.style.display = "block";
+	        console.log (e.target.src);
+	        imgModal.src = e.target.src;
+	        } else{
+	        	modal.style.display == "none";
+	        }
+	    });
+	}); 
