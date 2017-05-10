717ffaaecfff4fbb00560f1345409584c124cf45
Yelitza Cho <yelitza.choque@gmail.com>
2017-05-10 06:54:34 -0500

    otro commit

--- js/app.js
+++ js/app.js
@@ -1,3 +1,17 @@
-/*
- * Archivo principal de JS
- */
+var minScroll = 30;
+
+window.onscroll = function(){
+	var header = document.getElementsByTagName('header')[0];
+	var nav = document.getElementsByClassName('content-menu')[0];
+
+	var scroll = window.pageYOffset || document.documentElement.scrollTop;
+
+	if (scroll > 30) {
+		header.className = "";
+		nav.className = "";
+	} else {
+		header.className = "";
+		nav.className = "";
+	}
+}
+
